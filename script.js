8d02e212e7a2d74a8bbc58487d243ae9b3b496e2
Mohitpanwar09 <mohitpanwar9420@gmail.com>
2023-10-12 00:36:48 +0530

    made a div slider

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 
+const arr =["intro" , "anime"];
+let start = 0;
+
+
 function project_onclick(pjid)
 {
     //alert(pjid);
@@ -14,4 +18,43 @@ function project_onclick(pjid)
         prjId.style.display="block";
     }
     
+}
+
+
+
+function change_Content(arrowId)
+{
+    let id_content = arr[start];
+    var content_display = document.getElementById(id_content);
+
+    if(arrowId.toString()==="next_ptr")
+    {
+        if(start==(arr.length-1))
+        {
+            return;
+        }
+        
+        console.log("Hello")
+        content_display.style.display ="none";
+        start=start+1;
+        var content_to_disp = document.getElementById(arr[start]);
+        // console.log(content_to_disp);
+        content_to_disp.style.display ="block";
+    }
+    else
+    {
+        if(start==0)
+        {
+            return;
+        }
+        
+        let id_content = arr[start];
+        var content_display = document.getElementById(id_content);
+        content_display.style.display = "none";
+
+        start=start-1;
+        var content_to_disp = document.getElementById(arr[start]);
+        content_to_disp.style.display ="block";
+
+    }
 }
